77b1a5ad6717386278a0178e758e47c48619d038
Indra Ardiansah <indra.ardiansah@devixel.com>
2024-11-11 10:47:27 -0600

    change tag

--- src/root.tsx
+++ src/root.tsx
@@ -22,7 +22,7 @@ export default component$(() => {
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-M44RZPRT');`;
+    })(window,document,'script','dataLayer','GTM-NLBTMTDX');`;
 
   return (
     <QwikCityProvider>
